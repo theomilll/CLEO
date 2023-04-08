03f1717f5a9860a8190e68ce5f27eeed865b9e5b
theomilll <tam4@cesar.school>
2023-04-08 17:05:44 -0300

    .

--- main/views.py
+++ main/views.py
@@ -1,18 +1,38 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def login(request):
+    error_message = None
+
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        form_type = request.POST.get('form_type')
 
-        user = authenticate(request, username=username, password=password)
+        if form_type == 'login':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            error_message = "Invalid login credentials. Please try again."
-            return render(request, 'login.html', {'error_message': error_message})
-    else:
-        return render(request, 'login.html')
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                print("User is authenticated")
+                return redirect('home')
+            else:
+                print("User is not authenticated")
+                error_message = "Invalid login credentials. Please try again."
+
+        elif form_type == 'signup':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                print("User is created")
+                return redirect('login')
+            else:
+                error_message = "Invalid sign up information. Please try again."
+                print("User is not created")
+                print(form.errors)
+            error_message = "Invalid request."
+
+    form = SignUpForm()
+    return render(request, 'login.html', {'form': form, 'error_message': error_message})
