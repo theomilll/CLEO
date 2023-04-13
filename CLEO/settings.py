4bfb31ca9b58c23f32c02ef526e6840cf733342b
Cristina Matsunaga <criismnaga@gmail.com>
2023-04-12 21:42:15 -0300

    mais produtos adicionados no database

--- CLEO/settings.py
+++ CLEO/settings.py
@@ -116,8 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+
+#STATIC_ROOT = 'static'
+
 STATIC_URL = 'static/'
-STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
