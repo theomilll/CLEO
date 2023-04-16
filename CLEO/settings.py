04804a4aabdc29d134d45aa92ae2bc7149687f70
Maria Fernanda Freitas Barbosa Marques <mariafernandafreitasbarbosamarques@MacBook-Air-de-Maria-3.local>
2023-04-16 20:58:40 -0300

    .

--- CLEO/settings.py
+++ CLEO/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'CLEO.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
