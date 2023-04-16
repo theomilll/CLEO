771eaccb2cc81b4c950e89ee7084f5473c6f1da6
theomilll <tam4@cesar.school>
2023-04-16 14:40:35 -0300

    requisitos da senha adicionados

--- static/js/login.js
+++ static/js/login.js
@@ -9,3 +9,18 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+const passwordInput = $('input[name="password1"]');
+const passwordTooltip = $('#passwordTooltip');
+
+passwordInput.on('focus', () => {
+    passwordTooltip.css({
+    	display: 'block',
+    	left: passwordInput.position().left,
+      	top: passwordInput.position().top - passwordTooltip.outerHeight() - 10
+    });
+});
+
+passwordInput.on('blur', () => {
+    passwordTooltip.hide();
+});
