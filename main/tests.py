ffeb76c7a2b0e36ea1b993791fc5d1451b0c9bc6
Caiobadv <caiobarretodv@hotmail.com>
2023-04-26 20:50:08 -0300

    adicionando o selenium e começando a criar o codigo, ele ja esta abrindo a pagina

--- main/tests.py
+++ main/tests.py
@@ -23,21 +23,19 @@ class cleo(TestCase):
         confirm_register = driver.find_element_by_name("password2_r")
         confirm_register.click()
         confirm_register.send_keys("inevitavel40")
-
         botao_registrar = driver.find_element(By.XPATH, '//button[text()="Sign Up"]')
         botao_registrar.click()
 
         login = driver.find_element_by_id("signIn")
-        driver.execute_script("arguments[0].click();", login)
-
+        login.click()
         username = driver.find_element_by_name("username")
-        password = driver.find_element_by_name("password")
+        username.click()
         username.send_keys("thanos")
+        password = driver.find_element_by_name("password")
+        password.click()
         password.send_keys("inevitavel40")
-
         botao_login = driver.find_element(By.XPATH, '//button[text()="Log In"]')
         botao_login.click()
-
         adicionar_carrinho = driver.find_element_by_name("Adicionar ao carrinho")
         adicionar_carrinho.click()
 
