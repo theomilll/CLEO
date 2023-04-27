0d53e79bc2e33bc57f3b3b7903109f7df6800a8e
Caiobadv <caiobarretodv@hotmail.com>
2023-04-27 11:12:41 -0300

    ajeitando o seleneium, agora ele ja consegue apertar no botao e escrever as opçoes para registrar

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+from .forms import SignUpForm
 class cleo(TestCase):
     def test(self):
         chrome_options = webdriver.ChromeOptions()
@@ -8,35 +10,41 @@ class cleo(TestCase):
         driver = webdriver.Chrome(r"C:\Users\caiob\OneDrive\Área de Trabalho\driver chrome\chromedriver.exe", options=chrome_options)
 
         driver.get("http://127.0.0.1:8000/")
-
-        register = driver.find_element_by_id("signUp")
+        register = driver.find_element(By.ID,"signUp")
         register.click()
-        username_register = driver.find_element_by_name("username_r")
+        time.sleep(2)
+        username_register = driver.find_element(By.NAME,"username")
         username_register.click()
         username_register.send_keys("thanos")
-        email_register = driver.find_element_by_name("email_r")
+        time.sleep(1)
+        email_register = driver.find_element(By.NAME,"email")
         email_register.click()
         email_register.send_keys("thanos@gmail.com")
-        password_register = driver.find_element_by_name("password1_r")
+        time.sleep(1)
+        password_register = driver.find_element(By.NAME,"password1")
         password_register.click()
         password_register.send_keys("inevitavel40")
-        confirm_register = driver.find_element_by_name("password2_r")
+        time.sleep(1)
+        confirm_register = driver.find_element(By.NAME,"password2")
         confirm_register.click()
         confirm_register.send_keys("inevitavel40")
+        time.sleep(2)
         botao_registrar = driver.find_element(By.XPATH, '//button[text()="Sign Up"]')
         botao_registrar.click()
 
-        login = driver.find_element_by_id("signIn")
+        login = driver.find_element(By.ID,"signIn")
         login.click()
-        username = driver.find_element_by_name("username")
+        username = driver.find_element(By.ID,"username")
         username.click()
         username.send_keys("thanos")
-        password = driver.find_element_by_name("password")
+        time.sleep(2)
+        password = driver.find_element(By.ID,"password")
         password.click()
         password.send_keys("inevitavel40")
+        time.sleep(2)
         botao_login = driver.find_element(By.XPATH, '//button[text()="Log In"]')
         botao_login.click()
-        adicionar_carrinho = driver.find_element_by_name("Adicionar ao carrinho")
+        adicionar_carrinho = driver.find_element(By.NAME,"Adicionar ao carrinho")
         adicionar_carrinho.click()
-
+        time.sleep(2)
         driver.quit()
