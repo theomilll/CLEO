78225eaf4294fb87374997d2a5c7be2685e7c779
Caiobadv <caiobarretodv@hotmail.com>
2023-05-26 19:00:06 -0300

    tentativa 9 apos dicas de matheus

--- main/tests.py
+++ main/tests.py
@@ -107,16 +107,6 @@ class cleo(TestCase):
         confirmPurchase = driver.find_element(By.NAME, "generateQrCode")
         confirmPurchase.click()
         time.sleep(2)
-    def finalizar_pedido(self, driver):
-        FinalizarPedido=driver.find_element(By.ID, "confirmPurchase")
-        FinalizarPedido.click()
-    def voltar_catalogo(self, driver):
-        voltar_catalogo=driver.find_element(By.ID, "arrow")
-        voltar_catalogo.click()
-    def logout(self, driver):
-        logout= driver.find_element(By.ID,"log-out")
-        logout.click()
-        driver.quit()
     def run_tests(self, driver):
         self.register(driver)
         self.login(driver)
