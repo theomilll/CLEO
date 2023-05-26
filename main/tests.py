8e81039401431c8f298cf789a17057904ef4490f
TheoMoura <99195030+theomilll@users.noreply.github.com>
2023-05-26 16:46:52 -0300

    Update tests.py

--- main/tests.py
+++ main/tests.py
@@ -106,6 +106,7 @@ class cleo(TestCase):
         confirmPurchase = driver.find_element(By.NAME, "generateQrCode")
         confirmPurchase.click()
     def finalizar_pedido(self, driver):
+        time.sleep(5)
         FinalizarPedido=driver.find_element(By.NAME, "confirmPurchase")
         FinalizarPedido.click()
     def voltar_catalogo(self, driver):
