1f098a670b385e52cc8b0f19132c272e88937638
Caiobadv <caiobarretodv@hotmail.com>
2023-05-27 10:32:26 -0300

    tentativa 16 fazer bonitinho

--- main/tests.py
+++ main/tests.py
@@ -6,6 +6,7 @@ from .forms import SignUpForm
 from selenium.webdriver.common.action_chains import ActionChains
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 class cleo(TestCase):
