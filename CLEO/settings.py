d5eaa2043fd47070831e1ff05ec0037ab1506283
Caiobadv <caiobarretodv@hotmail.com>
2023-06-09 18:06:45 -0300

    deploy automatizado

--- CLEO/settings.py
+++ CLEO/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'CLEO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
